a50d442857f978a8fccb24f451ef15edb8b3965a
AlenHuang MBP <alenhung@gmail.com>
2018-03-29 17:21:14 +0800

    HTML修訂，符合Bulma格式

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,73 +1,142 @@
-@extends('layouts.app')
+@extends('layouts.site')
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+    <div class="columns is-centered">
+        <div class="column is-two-thirds m-t-80">
+            <div class="card">
+                <div class="card-content">
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                    <h1 class="is-1">註冊使用者</h1>
+
+                    <form class="form-horizontal m-r-15 m-l-15" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
+                        <!---->
+                        <div class="field is-horizontal">
+                          <div class="field-label">
+                            <label class="label">姓氏</label>
+                          </div>
+                          <div class="field-body">
+                            <div class="field">
+                              <div class="field">
+                                <p class="control">
+                                  <input id="firstName" type="text" class="input" name="firstName" value="{{ old('firstName') }}" required autofocus>
+                                </p>
+                              </div>
+                              @if ($errors->has('firstName'))
+                              <p class="help">{{ $errors->first('firstName') }}</p>
+                              @endif
                             </div>
+                          </div>
                         </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                        <!---->
+                        <!---->
+                        <div class="field is-horizontal">
+                          <div class="field-label">
+                            <label class="label">名</label>
+                          </div>
+                          <div class="field-body">
+                            <div class="field">
+                              <div class="field">
+                                <p class="control">
+                                  <input id="lastName" type="text" class="input" name="lastName" value="{{ old('lastName') }}" required autofocus>
+                                </p>
+                              </div>
+                              @if ($errors->has('lastName'))
+                              <p class="help">{{ $errors->first('lastName') }}</p>
+                              @endif
                             </div>
+                          </div>
                         </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                        <!---->
+                        <!---->
+                        <div class="field is-horizontal">
+                          <div class="field-label">
+                            <label class="label">使用者名稱</label>
+                          </div>
+                          <div class="field-body">
+                            <div class="field">
+                              <div class="field">
+                                <p class="control">
+                                  <input id="name" type="text" class="input" name="name" value="{{ old('name') }}" required autofocus>
+                                </p>
+                              </div>
+                              @if ($errors->has('name'))
+                              <p class="help">{{ $errors->first('name') }}</p>
+                              @endif
                             </div>
+                          </div>
                         </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                        <!---->
+                        <!---->
+                        <div class="field is-horizontal">
+                          <div class="field-label">
+                            <label class="label">公司電子信箱</label>
+                          </div>
+                          <div class="field-body">
+                            <div class="field">
+                              <div class="field">
+                                <p class="control">
+                                  <input id="email" type="email" class="input" name="email" value="{{ old('email') }}" required>
+                                </p>
+                              </div>
+                              @if ($errors->has('email'))
+                              <p class="help">{{ $errors->first('email') }}</p>
+                              @endif
                             </div>
+                          </div>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
+                        <!---->
+                        <!---->
+                        <div class="field is-horizontal">
+                          <div class="field-label">
+                            <label class="label">密碼</label>
+                          </div>
+                          <div class="field-body">
+                            <div class="field">
+                              <div class="field">
+                                <p class="control">
+                                  <input id="password" type="password" class="input" name="password" required>
+                                </p>
+                              </div>
+                              @if ($errors->has('password'))
+                              <p class="help">{{ $errors->first('password') }}</p>
+                              @endif
+                            </div>
+                          </div>
+                        </div>
+                        <!---->
+                        <!---->
+                        <div class="field is-horizontal">
+                          <div class="field-label">
+                            <label class="label">確認密碼</label>
+                          </div>
+                          <div class="field-body">
+                            <div class="field">
+                              <div class="field">
+                                <p class="control">
+                                  <input id="password-confirm" type="password" class="input" name="password-confirm" required>
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <!---->
+                        <div class="field is-horizontal">
+                          <div class="field-label">
+                            <!-- Left empty for spacing -->
+                          </div>
+                          <div class="field-body">
+                            <div class="field">
+                              <div class="control">
+                                <button class="button is-primary">
+                                  註冊
                                 </button>
+                              </div>
                             </div>
+                          </div>
                         </div>
+
                     </form>
                 </div>
             </div>
