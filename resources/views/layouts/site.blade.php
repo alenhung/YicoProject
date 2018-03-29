75595782bca35b5ec4f444ea7322e7db73bfa1cd
AlenHuang MBP <alenhung@gmail.com>
2018-03-29 15:27:36 +0800

    layout 設計
    定義Layout樣板

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -4,34 +4,25 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>{{ config('app.name', 'Laravel') }}</title>
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     @if (Auth::guest())
     @else
-    <div id="app">
       @include('_includes.nav.main')
-      <div class="container is-fluid">
-        <div class="columns">
-          <div class="column is-narrow is-2">
-            @section('sidebar')
-                <p>側邊欄 sidebar</p>
-            @show
-          </div>
-          <div id="content" class="column is-10 m-t-20 m-b-20">
-            @yield('content')
-          </div>
-        </div>
     @endif
-    @include('_includes.footer.main')
+    <div id="app">
+      @yield('content')
     </div>
-  </div>
+    @include('_includes.footer.main')
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
-  @yield('scripts')
 </body>
 </html>
