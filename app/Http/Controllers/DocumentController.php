082c597c690530733fba8cc565544f26aa5d5106
AlenHuang MBP <alenhung@gmail.com>
2018-04-01 15:44:32 +0800

    建立Controller頁面連結

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -14,6 +14,7 @@ class DocumentController extends Controller
     public function index()
     {
         //
+        return view('document/index');
     }
 
     /**
@@ -24,6 +25,7 @@ class DocumentController extends Controller
     public function create()
     {
         //
+        return view('document/create');
     }
 
     /**
